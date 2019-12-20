f63f8b549c7109cbbe3cdeb08c4108d65c7675bd
ljj <ljj@yimuzn.com>
2019-12-20 13:07:38 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-## title
+## git使用快捷键
+* commit: Ctrl+k
+* push: Ctrl+Shift+k
+* 代码更改完毕要commit再push
+## 与已经提交代码的对比
+* Version Commit 里面的列表右键点击Compare with local
--- src/main/java/example/Test.java
+++ src/main/java/example/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("1244");
         System.out.println("12442we3");
+        System.out.println("12442we31232K");
     }
 }
